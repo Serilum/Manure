7b0f06671639adf98937a6e1f4de2b6b689b9a56
Rick South <rick@natamus.com>
2024-06-23 01:27:02 +0200

    (v3.5) Simplified registering items with Collective's helper functions.

--- Fabric/src/main/java/com/natamus/manure/ModFabric.java
+++ Fabric/src/main/java/com/natamus/manure/ModFabric.java
@@ -1,25 +1,16 @@
 package com.natamus.manure;
 
 import com.natamus.collective.check.RegisterMod;
-import com.natamus.manure.dispenser.RecipeManager;
 import com.natamus.manure.events.ManureDropEvent;
-import com.natamus.manure.items.ManureItems;
 import com.natamus.manure.util.Reference;
 import com.natamus.manure.util.Util;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.BoneMealItem;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 
 public class ModFabric implements ModInitializer {
 	
@@ -27,21 +18,12 @@ public class ModFabric implements ModInitializer {
 	public void onInitialize() {
 		setGlobalConstants();
 		ModCommon.init();
+		ModCommon.registerAssets(null);
+		ModCommon.setAssets();
 
-		registerItems();
 		loadEvents();
 
 		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
-	}
-
-	@SuppressWarnings("UnstableApiUsage")
-	private void registerItems() {
-		ManureItems.MANURE = new BoneMealItem(new Item.Properties());
-
-		Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "manure"), ManureItems.MANURE);
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.prepend(ManureItems.MANURE));
-
-		RecipeManager.initDispenserBehavior();
 	}
 
 	private void loadEvents() {
