64889b79d29cc1989e64ff63bb747aecfaed7905
Rick South <rick@natamus.com>
2024-06-23 01:26:47 +0200

    (v3.5) Simplified registering items with Collective's helper functions.

--- Fabric/src/main/java/com/natamus/manure/ModFabric.java
+++ Fabric/src/main/java/com/natamus/manure/ModFabric.java
@@ -1,43 +1,29 @@
 package com.natamus.manure;
 
 import com.natamus.collective.check.RegisterMod;
-import com.natamus.manure.dispenser.RecipeManager;
 import com.natamus.manure.events.ManureDropEvent;
-import com.natamus.manure.items.ManureItems;
 import com.natamus.manure.util.Reference;
 import com.natamus.manure.util.Util;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.BoneMealItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 
 public class ModFabric implements ModInitializer {
-	
+
 	@Override
 	public void onInitialize() {
 		setGlobalConstants();
 		ModCommon.init();
+		ModCommon.registerAssets(null);
+		ModCommon.setAssets();
 
-		registerItems();
 		loadEvents();
 
 		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
-	}
-
-	private void registerItems() {
-		ManureItems.MANURE = new BoneMealItem(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS));
-
-		Registry.register(Registry.ITEM, new ResourceLocation(Reference.MOD_ID, "manure"), ManureItems.MANURE);
-
-		RecipeManager.initDispenserBehavior();
 	}
 
 	private void loadEvents() {
