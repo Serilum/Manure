581e62e8dbb506e52931d03855268b62176052fd
Rick South <rick@natamus.com>
2026-04-10 02:51:08 +0200

    Manure source code for 26.1.2.

---
+++ Forge/src/main/java/com/natamus/manure/forge/events/ForgeManureDropEvent.java
@@ -0,0 +1,56 @@
+package com.natamus.manure.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.manure.events.ManureDropEvent;
+import com.natamus.manure.util.Util;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.EntityLeaveLevelEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeManureDropEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeManureDropEvent.class);
+	}
+
+    @SubscribeEvent
+    public static void onWorldLoad(LevelEvent.Load e) {
+        Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+        if (level == null) {
+            return;
+        }
+
+        Util.attemptBlacklistProcessing(level);
+    }
+
+    @SubscribeEvent
+    public static void onServerTick(TickEvent.ServerTickEvent.Post e) {
+        ManureDropEvent.onServerTick(e.server());
+    }
+
+    @SubscribeEvent
+    public static void onEntityJoin(EntityJoinLevelEvent e) {
+        Level level = e.getLevel();
+        if (level.isClientSide()) {
+            return;
+        }
+
+        ManureDropEvent.onEntityJoin(e.getEntity(), (ServerLevel)level);
+    }
+
+    @SubscribeEvent
+    public static void onEntityLeave(EntityLeaveLevelEvent e) {
+        Level level = e.getLevel();
+        if (level.isClientSide()) {
+            return;
+        }
+
+        ManureDropEvent.onEntityLeave(e.getEntity(), (ServerLevel)level);
+    }
+}
